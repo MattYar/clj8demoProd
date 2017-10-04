8f14092aadf64bda20a9d601f76e081f293a85d6
Colten Krauter <ckrauter@starbucks.com>
2017-10-04 06:21:29 -0700

    Updating

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
-echo 'hi
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>US WEST - PROD</title>
+<title>Cloud Project - Team 8</title>
 <link href="style.css" rel="stylesheet">
+<script src="bower_components\jquery\dist\jquery.min.js"></script>
+<script type="text/javascript">
+    $(function () {
+        var interval = setInterval(function() { 
+            $.post("ping.php", function(data, status){
+                $("#ping-east").append(data +'<br>');
+            });
+        }, 2000);
+    });
+</script>
 </head>
 
 <body>
-    <h1>US WEST - PROD</h1>
-<img src="us.png" id=us>
+    <h1></h1>
+    <img src="us.png" id=us>
+    <div id="ping-east"></div>
+    <div id="ping-west"></div>
 </body>
 
 </html>
