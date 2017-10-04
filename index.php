2d0800828b0c3804296d15f9b3c5c5459b98c9a2
Colten Krauter <ckrauter@starbucks.com>
2017-10-04 05:23:50 -0700

    Adding new version

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+
 ?>
-USWEST - PROD
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>US WEST - PROD</title>
+<link href="style.css" rel="stylesheet">
+</head>
+
+<body id=west>
+    <h1 id=west>US WEST - PROD</h1>
+<img src="us.png" id=us>
+</body>
+
+</html>
+
+
